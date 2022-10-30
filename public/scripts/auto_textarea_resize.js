e6b43b2512e393f32320dbff2d7ef7086370e37f
harle <harleyharris05@hotmail.com>
2022-10-30 18:55:09 +0000

    textarea auto sizes on focus

--- public/scripts/auto_textarea_resize.js
+++ public/scripts/auto_textarea_resize.js
@@ -3,20 +3,10 @@ function auto_resize_textareas() {
 
     for (const textarea of textareas) {
         textarea.style.csstext = `height: ${textarea.scrollHeight}px; overflow-y: hidden`;
-        textarea.addEventListener("input", function () {
+        textarea.addEventListener("focus", function () {
             this.style.height = "auto";
             this.style.height = `${this.scrollHeight}px`;
         });
-    }
-}
-
-window.onload = () => {
-    const textareas = document.getElementsByTagName("textarea");
-
-    for (const textarea of textareas) {
-        textarea.style.csstext = `height: ${textarea.scrollHeight}px; overflow-y: hidden`;
-        textarea.style.height = "auto";
-        textarea.style.height = `${this.scrollHeight}px`;
     }
 }
 
